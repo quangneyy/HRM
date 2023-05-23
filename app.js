eac3767f6fb72d4593d1cf7432abdd8eced93c72
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-05-23 22:46:36 +0700

    add-passport

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const express = require("express");
-const path = require("path");
-const favicon = require("serve-favicon");
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-const port = 8080;
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+var MongoStore = require("connect-mongo")(session);
 
-const app = express();
+var index = require("./routes/index");
+const port = 8000;
+
+expressValidator = require("express-validator");
+
+mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/HRMS');
+var mongoDB = "mongodb://localhost:27017/HRM";
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+var db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("MongoDB connection failed...");
+});
+db.once("open", () => {
+  console.log("MongoDB connection successful...");
+});
+require("./config/passport.js");
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,9 +45,49 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//validator should be ater body parser
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+    cookie: { maxAge: 180 * 60 * 1000 },
+  })
+);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use("/", (req, res) => {
-  res.render("<h1>Cam on</h1>");
+app.use("/", index);
+
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  res.locals.messages = req.flash();
+  next();
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(port, () => {
