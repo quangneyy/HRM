ec5c92899369fe8d6e994779a23d7683fefcad76
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-05-25 15:45:08 +0700

    add-viewProfileAdmin

--- routes/admin.js
+++ routes/admin.js
@@ -32,6 +32,23 @@ router.get("/", function viewHome(req, res, next) {
   });
 });
 
+router.get("/view-profile", function viewProfile(req, res, next) {
+  User.findById(req.session.user._id, function getUser(err, user) {
+    if (err) {
+      console.log(err);
+    }
+    res.render("Admin/viewProfile", {
+      title: "Profile",
+      csrfToken: req.csrfToken(),
+      employee: user,
+      moment: moment,
+      userName: req.session.user.name,
+    });
+  });
+});
+
+module.exports = router;
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
